8f46a4ed9808da11778640172257af1567e763e3
d.takacs <d.takacs@mortoff.hu>
2016-08-25 21:04:14 +0200

    windowing added

--- PMRegression.R
+++ PMRegression.R
@@ -17,6 +17,7 @@ colnames(sampleFull)[2] <- "TimeToFailure"
 #create label for TimeToFailure
 
 labelVector <- c(seq(6361,1),sampleFull$TimeToFailure[(6361+1):nrow(sampleFull)])
+labelVector <- c(seq(5705,1),sampleFull$TimeToFailure[(5705+1):nrow(sampleFull)])
 sampleFull$TimeToFailure <- labelVector
 
 # Randomize 
@@ -29,15 +30,9 @@ sampleTrainLabels <- sampleRandom$TimeToFailure[samplingVector]
 sampleTest <- sampleRandom[-samplingVector,]
 sampleTestlabels <- sampleRandom$TimeToFailure[-samplingVector]
 
-#PCA
-
-
 #principal component analysis
 prin_comp <- prcomp(sampleTrain[,c(-1,-2)], scale. = T)
 names(prin_comp)
-prin_comp$rotation[1:10,1:8]
-
-biplot(prin_comp, scale = 0)
 
 #compute standard deviation of each principal component
 std_dev <- prin_comp$sdev
@@ -54,96 +49,173 @@ prop_varex[1:20]
 
 #scree plot
 plot(prop_varex, xlab = "Principal Component",
-       ylab = "Proportion of Variance Explained",
-       type = "b")
+     ylab = "Proportion of Variance Explained",
+     type = "b")
 
 #cumulative scree plot
-plot(cumsum(prop_varex), xlab = "Principal Component",
-       ylab = "Cumulative Proportion of Variance Explained",
-       type = "b")
+plot(cumsum(prop_varex),
+     xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     type = "b")
 
 #add a training set with principal components
-train.data <- data.frame(RemainingTime = sampleTrain$TimeToFailure, prin_comp$x)
-
-#we are interested in first 30 PCAs
-train.data <- train.data[,1:600]
-
-#transform test into PCA
-test.data <- predict(prin_comp, newdata = sampleTest)
-test.data <- as.data.frame(test.data)
+sampleTrainPCA <- data.frame(TimeToFailure = sampleTrain$TimeToFailure, prin_comp$x)
 
 #select the first 600 components
-test.data <-test.data[,1:600]
+sampleTrainPCA <- sampleTrainPCA[,1:600]
 
-sampleModelWithPCa <- lm(RemainingTime ~., data = train.data)
-samplePredictionsWithPCa <- predict(sampleModelWithPCa, test.data)
+#transform test into PCA
+sampleTestPCA <- predict(prin_comp, newdata = sampleTest)
+sampleTestPCA <- as.data.frame(sampleTestPCA)
 
-compute_rmse <- function(predictions, actual) { 
-  mean( sqrt((predictions - actual) ^ 2 )) 
-}
+#select the first 600 components
+sampleTestPCA <-sampleTestPCA[,1:600]
 
-compute_rmse(round(samplePredictionsWithPCa), sampleTest$TimeToFailure)
+#################################### Linear Regression ####################################
 
-comparisonWithPCA <- cbind.data.frame(Time = sampleTest$timeMs,
-                               Predictions = round(samplePredictionsWithPCa),
+modelLinearRegressionPCA <- lm(RemainingTime ~., data = sampleTrainPCA)
+predictionsLinearRegressionPCA <- predict(modelLinearRegressionPCA, sampleTestPCA)
+
+compute_rmse(round(predictionsLinearRegressionPCA), sampleTest$TimeToFailure)
+
+resultLinearRegressionPCA <- cbind.data.frame(Time = sampleTest$timeMs,
+                               Predictions = round(predictionsLinearRegressionPCA),
                                Actual = sampleTest$TimeToFailure,
-                               Difference = abs(round(samplePredictionsWithPCa)-sampleTest$TimeToFailure))
+                               Difference = abs(round(predictionsLinearRegressionPCA)-sampleTest$TimeToFailure))
+#print median
+median(resultLinearRegressionPCA$Difference)
 
-medianWithPca <- median(comparisonWithPCA$Difference)
-
-plot1 <- ggplot(data=comparisonWithPCA, aes(x=Time, y=Difference)) +
+#plot Actual-Predicted Difference
+ggplot(data=resultLinearRegressionPCA, aes(x=Time, y=Difference)) +
   geom_line() +
-  geom_point() + geom_vline(xintercept = 542979,colour="red",linetype = "longdash",size = 1,5) +
+  geom_point() + 
+  geom_vline(xintercept = 487000,colour="red",linetype = "longdash",size = 1,5) +
   geom_hline(yintercept = median(comparisonWithPCA$Difference),colour="darkgoldenrod1",linetype = "longdash",size = 1,5) +
   geom_text(aes(0,median(comparisonWithPCA$Difference),label = round(median(comparisonWithPCA$Difference)), vjust = -0.5,hjust = 1.2)) +
-  scale_x_continuous(breaks=seq(0,600000,60000)) + ggtitle("with PCA")
+  scale_x_continuous(breaks=seq(0,600000,60000)) + ggtitle("Linear Regression with PCA")
 
 
-###################################### scale ######################################
-sampleTrainScaled <- sampleTrain
-sampleTestScaled <- sampleTest
-sampleTrainScaled[,c(-1,-2)] <- as.data.frame(lapply(sampleTrain[,c(-1,-2)], normalize) )
-sampleTestScaled[,c(-1,-2)] <- as.data.frame(lapply(sampleTest[,c(-1,-2)], normalize)) 
+#################################### Neural Network without PCA ####################################
+
+modelNeuralNetwork <- nnet(TimeToFailure~.,data=sampleTrain ,size =5,MaxNWts=22000,linout=TRUE)
+
+predictionsNeuralNetwork<-predict(modelNeuralNetwork ,sampleTest ,type = "raw")
+
+compute_rmse(round(predictionsNeuralNetwork), sampleTest$TimeToFailure)
+
+resultNeuralNetwork <- cbind.data.frame(Time = sampleTest$timeMs,
+                                   Predictions = round(predictionsNeuralNetwork),
+                                   Actual = sampleTest$TimeToFailure,
+                                   Difference = abs(round(predictionsNeuralNetwork)-sampleTest$TimeToFailure))
 
 
-sampleModel <- lm(TimeToFailure ~., data = sampleTrain[,2:ncol(sampleTrain)])
-samplePredictions <- predict(sampleModel, sampleTest[,2:ncol(sampleTest)])
-
-compute_rmse <- function(predictions, actual) { 
-  mean( sqrt((predictions - actual) ^ 2 )) 
-}
-
-compute_rmse(round(samplePredictions), sampleTest$TimeToFailure)
-
-comparison <- cbind.data.frame(Time = sampleTest$timeMs,
-                               Predictions = round(samplePredictions),
-                               Actual = sampleTest$TimeToFailure,
-                               Difference = abs(round(samplePredictions)-sampleTest$TimeToFailure))
-
-
-plot2 <- ggplot(data=comparisonNNet, aes(x=Time, y=Difference)) +
+ggplot(data=resultNeuralNetwork, aes(x=Time, y=Difference)) +
   geom_line() +
-  geom_point() + geom_vline(xintercept = 542979,colour="red",linetype = "longdash",size = 1,5) +
-  geom_hline(yintercept = median(comparisonNNet$Difference),colour="darkgoldenrod1",linetype = "longdash",size = 1,5) +
-  geom_text(aes(0,median(comparisonNNet$Difference),label = round(median(comparisonNNet$Difference)), vjust = -0.5,hjust = 1.2)) +
-  scale_x_continuous(breaks=seq(0,600000,60000)) + ggtitle("without PCA")
+  geom_point() + 
+  geom_vline(xintercept = 487000,colour="red",linetype = "longdash",size = 1,5) +
+  geom_hline(yintercept = median(resultNeuralNetwork$Difference),colour="darkgoldenrod1",linetype = "longdash",size = 1,5) +
+  geom_text(aes(0,median(resultNeuralNetwork$Difference),label = round(median(resultNeuralNetwork$Difference)), vjust = -0.5,hjust = 1.2)) +
+  scale_x_continuous(breaks=seq(0,600000,60000)) + ggtitle("Neural Network")
 
 
-multiplot(plot1,plot2,cols = 1)
+#################################### Neural Network with PCA ####################################
 
+modelNeuralNetworkPCA <- nnet(TimeToFailure~.,data=sampleTrainPCA ,size =10,MaxNWts=22000,linout=TRUE)
 
-# Neural  network: 10 nodes  in the  hidden  layer
+predictionsNeuralNetworkPCA<-predict(modelNeuralNetworkPCA ,sampleTestPCA ,type = "raw")
 
-NN_model <-nnet(TimeToFailure~.,data=sampleTrain ,size =10,MaxNWts=22000,linout=TRUE)
-
-classification_NN<-predict(NN_model ,sampleTest ,type = "raw")
-
-compute_rmse(round(classification_NN), sampleTest$TimeToFailure)
+compute_rmse(round(predictionsNeuralNetworkPCA), sampleTest$TimeToFailure)
 
 comparisonNNet <- cbind.data.frame(Time = sampleTest$timeMs,
                                Predictions = round(classification_NN),
                                Actual = sampleTest$TimeToFailure,
                                Difference = abs(round(classification_NN)-sampleTest$TimeToFailure))
 
-median(comparisonNNet$Difference)
+#################################### window ####################################
+row <- vector()
+df <- data.frame()
+sampleTemp <- sampleFull[,c(-1,-2)]
+createNewDataFrame <- function(n){
+  row <- vector()
+  df <<- data.frame()
+  for (i in 1:nrow(sampleTemp) ){
+    row <- c(row,sampleTemp[i,])
+    
+    if (i %% n == 0) {
+      names(row) <- NULL
+      df <<- rbind.data.frame(df,row)
+      row <- vector()
+      print(paste("Row = ",i,"/",nrow(sampleTemp),sep=""))
+    }
+  }
+}
 
+createNewDataFrame(110)
+
+#create labels
+
+ttf <- c(seq(57,0),0,0,0,0,0)
+
+df$ttf <- ttf
+
+
+
+# Randomize 
+sampleRandom <- df[sample(1:nrow(df)),]
+
+#split dataset
+samplingVector <- createDataPartition(df$ttf, p = 0.7, list = FALSE)
+sampleTrain <- sampleRandom[samplingVector,]
+sampleTrainLabels <- sampleRandom$TimeToFailure[samplingVector]
+sampleTest <- sampleRandom[-samplingVector,]
+sampleTestlabels <- sampleRandom$TimeToFailure[-samplingVector]
+
+dfPCA <- prcomp(sampleTrain[,-length(colnames(df))], scale. = T)
+
+#add a training set with principal components
+sampleTrainPCA <- data.frame( TimeToFailure = sampleTrain$ttf, dfPCA$x)
+
+#compute standard deviation of each principal component
+std_dev <- dfPCA$sdev
+
+#compute variance
+pr_var <- std_dev^2
+
+#check variance of first 10 components
+pr_var[1:10]
+
+#proportion of variance explained
+prop_varex <- pr_var/sum(pr_var)
+prop_varex[1:20]
+
+#scree plot
+plot(prop_varex, xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     type = "b")
+
+#cumulative scree plot
+plot(cumsum(prop_varex),
+     xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     type = "b")
+
+#select the first 600 components
+sampleTrainPCA <- sampleTrainPCA[,1:40]
+
+#transform test into PCA
+sampleTestPCA <- predict(dfPCA, newdata = sampleTest)
+sampleTestPCA <- as.data.frame(sampleTestPCA)
+
+#select the first 600 components
+sampleTestPCA <-sampleTestPCA[,1:40]
+
+modelNeuralNetworkPCA <- nnet(TimeToFailure~.,data=sampleTrainPCA ,size =10,MaxNWts=22000,linout=TRUE)
+
+predictionsNeuralNetworkPCA<-predict(modelNeuralNetworkPCA ,sampleTestPCA ,type = "raw")
+
+compute_rmse(round(predictionsNeuralNetworkPCA), sampleTest$ttf)
+
+comparisonNNet <- cbind.data.frame(
+                                   Predictions = round(predictionsNeuralNetworkPCA),
+                                   Actual = sampleTest$ttf,
+                                   Difference = abs(round(predictionsNeuralNetworkPCA)-sampleTest$ttf))
